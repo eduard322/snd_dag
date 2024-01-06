b756ccc279856a0d36be45806aeec5a020ad4b20
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-01-06 18:47:43 +0100

    Add DAG

--- generate_input_file.sh
+++ generate_input_file.sh
@@ -5,15 +5,14 @@ GEOFILE="/afs/cern.ch/user/o/olantwin/SND/geofile.050124.gdml"
 MPL="/afs/cern.ch/user/o/olantwin/SND/mpl.xml"
 XSECTION="/afs/cern.ch/user/o/olantwin/public/genie_splines_GENIE_v32_ADVSNDG18_02a_01_000_2.xml"
 FLUX="/afs/cern.ch/user/o/olantwin/public/HL-LHC_neutrinos_TI18_20e6pr_gsimple.root"
-NEVENTS=$3
-OUTPUTDIR=
+NEVENTS=$2
 TUNE=SNDG18_02a_01_000
 EVENTGENLIST=CCDIS
 TOPVOLUME="+volAdvTarget"
 OUTPUTFILE="sndlhc_"$TOPVOLUME"_"$NEVENTS"_ADV"$TUNE".0.ghep.root"
 NEUTRINO=14
 
-ProcId=$2
+ProcId=$1
 LSB_JOBINDEX=$((ProcId+1))
 SEED=$LSB_JOBINDEX
 
